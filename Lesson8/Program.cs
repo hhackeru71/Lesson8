97e9b2fdc3c74e404392b5ace35d7472b6168bdc
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-10-03 18:06:57 +0300

    .

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -6,7 +6,18 @@ namespace Lesson8
     {
         static void Main(string[] args)
         {
-            Pt2();
+            //Pt2();
+            while (true)
+            {
+                Console.WriteLine("Please ente a num ");
+                int.TryParse(Console.ReadLine(), out int number5);
+                Console.WriteLine(IsPrime(number5));
+
+            }
+
+            Pt3A();
+
+
 
             //int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
             ////foreach(int index in arr)
@@ -25,13 +36,53 @@ namespace Lesson8
             //}
         }
 
+        static void Pt3A()
+        {
+            Console.WriteLine("________________TR3 sol 2________________");
+            Console.WriteLine("please enter a number");
+            int.TryParse(Console.ReadLine(), out int num);
+            int devisors = 0;
+            if (num <= 0)
+            {
+                Console.WriteLine("please enter a valid number");
+            }
+            else
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                        devisors++;
+                }
+            }
+            if (devisors == 2)
+            {
+                Console.WriteLine("is a prime");
+            }
+            else
+            {
+                Console.WriteLine("is not prime");
+            }
+            Console.WriteLine("________________TR3 sol 2________________");
+        }
         static void Pt2()
         {
             Console.WriteLine("enter a number");
             int.TryParse(Console.ReadLine(), out int number);
             Console.WriteLine($"number between 1 -10 {PrintNumber1to10(number)}");
         }
+        static bool IsPrime(int number5)
+        {
 
+            if ((number5 % 2 == 0 && number5 != 2) || (number5 % 3 == 0 && number5 != 3))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
         static bool PrintNumber1to10(int until)
         {
             for (int i = 1; i <= until; i++)
